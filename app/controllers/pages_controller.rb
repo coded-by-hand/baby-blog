583836cc189dce706db326f305ffab87c18bbcb1
Jack Boberg <jack.boberg@gmail.com>
2014-02-10 20:31:33 -0500

    Allow custom layouts per page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,24 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
+
+  layout :layout_for_page
+
+  private
+
+    def layout_for_page
+      # tries to use a matching app/views/layouts/page_title.html.erb
+      if template_exists? "layouts/#{params[:id]}"
+        params[:id]
+      else
+        'application'
+        # # use specific overrides
+        # case params[:id] # page_title
+        # when 'example'
+        #   'some_shared_layout'
+        # else
+        #   'application'
+        # end
+      end
+    end
+
 end
